7c073048108c6714945509396dbddedac3f790a3
Eli-PC\Eli <elenashumanova@yahoo.com>
2016-10-23 19:31:36 +0100

    Homework 2 last tasks 23.10.2016

---
+++ IsoscelesTriangle/IsoscelesTriangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace IsoscelesTriangle
+{
+    class IsoscelesTriangle
+    {
+        static void Main()
+        {
+            char symbol= (char)251;
+            Console.WriteLine(symbol);
+            }
+        }
+    }
+}
