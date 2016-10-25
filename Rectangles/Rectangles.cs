4b0be17a0a98cc60c6d4d484a31019431b73e0d3
Eli-PC\Eli <elenashumanova@yahoo.com>
2016-10-25 19:04:44 +0100

    Update on homework 25.10.2016

---
+++ Rectangles/Rectangles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Rectangles
+{
+    class Rectangles
+    {
+        static void Main()
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double rectangularArea = width * height;
+            double rectangularPerimeter = (2 * width) + (2 * height);
+            Console.WriteLine("{0:F2} {1:F2}", rectangularArea,  rectangularPerimeter);
+        }
+    }
+}
---
+++ ThirdDigit/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ThirdDigit
+{
+    class Startup
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            int result = ((N / 100) % 10);
+            if (result == 7)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false {0}", result);
+            }
+        }
+    }
+}
