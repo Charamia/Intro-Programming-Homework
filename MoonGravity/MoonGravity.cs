23667f8022b676972337a6ea48c84a58c01e5cdf
Eli-PC\Eli <elenashumanova@yahoo.com>
2016-10-25 07:03:02 +0100

    24.05.2016 update

---
+++ MoonGravity/MoonGravity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace MoonGravity
+{
+    class MoonGravity
+    {
+        static void Main()
+        {
+            float weightOnTheEarth = float.Parse(Console.ReadLine());
+            float weightOnTheMoon = weightOnTheEarth * 0.17f;
+            Console.WriteLine("{0:0.000}", weightOnTheMoon);
+        }
+    }
+}
---
+++ OddOrEven/OddOrEven.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace OddOrEven
+{
+    class OddOrEven
+    {
+        static void Main()
+        {
+            int min = -30;
+            int max = 30;
+            int chislo = int.Parse(Console.ReadLine());
+            if (chislo % 2 ==0)
+            {
+                Console.WriteLine("even " + chislo);
+            }
+            else
+            {
+                Console.WriteLine("odd " + chislo);
+            }
+
+
+        }
+    }
+}
